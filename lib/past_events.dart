7c22f26ce3d1ce5dd1713231c203e0b5c4dab493
Neha Aggarwal <neha68977@gmail.com>
2020-10-17 01:40:53 +0530

    Past Event Page updated

--- lib/past_events.dart
+++ lib/past_events.dart
@@ -22,7 +22,7 @@ class _PastEventPageState extends State<PastEventPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Past Event Page'),
+        title: Text('Past Event Page', style: GoogleFonts.alegreya(fontSize: 25.0),),
         backgroundColor: Color(0xff25bcbb),
       ),
       backgroundColor: Color(0xff18203d),
@@ -34,76 +34,101 @@ class _PastEventPageState extends State<PastEventPage> {
             padding: EdgeInsets.all(10.0),
 //            height: 220,
             width: double.maxFinite,
-            child: Card(
-              elevation: 5.0,
-              child: Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
+            child: Column(
+              children: [
+                _buildCard("Hackathon","by CSI-BPIT","assets/images/nature.jpg", "Hackathon is the platform where in groups we showcase our ability and creativity."),
+                SizedBox(height: 10.0,),
+                _buildCard("Hackathon","by CSI-BPIT","assets/images/nature.jpg", "Hackathon is the platform where in groups we showcase our ability and creativity."),
+                SizedBox(height: 10.0,),
+                _buildCard("Hackathon","by CSI-BPIT","assets/images/nature.jpg", "Hackathon is the platform where in groups we showcase our ability and creativity."),
+                SizedBox(height: 10.0,),
+                _buildCard("Hackathon","by CSI-BPIT","assets/images/nature.jpg", "Hackathon is the platform where in groups we showcase our ability and creativity."),
+                SizedBox(height: 10.0,),
+                _buildCard("Hackathon","by CSI-BPIT","assets/images/nature.jpg", "Hackathon is the platform where in groups we showcase our ability and creativity."),
 
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-
-                      children: [
-                        Container(
-                          height: 80.0,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage('assets/images/nature.jpg'),
-//                              fit: BoxFit.fitWidth,
-                                alignment: Alignment.topLeft,
-                              )
-                          ),
-                        ),
-                        Column(
-                          children: [
-                            Text("Event Name", style: GoogleFonts.lobster(fontSize: 20.0),),
-                            Text("Organization", style: GoogleFonts.lobster(fontSize: 10.0))
-                          ],
-                        ),
-
-                      ],
-                    ),
-
-                    Text("Description Description Description Description Description Description"),
-                    FlatButton(
-                      textColor: Colors.green,
-                      child: Text("Read More"),
-                      onPressed: null,
-                    ),
-                    ButtonBar(
-                      alignment: MainAxisAlignment.end,
-                      children: [
-                        IconButton(
-                          icon: Icon( FontAwesomeIcons.facebook,),
-                          onPressed: null,
-                        ),
-                        IconButton(
-                          icon: Icon( FontAwesomeIcons.chrome,),
-                          onPressed: null,
-                        ),
-                        IconButton(
-                          icon: Icon( FontAwesomeIcons.twitter,),
-                          onPressed: null,
-                        ),
-                        IconButton(
-                          icon: Icon( FontAwesomeIcons.instagram,),
-                          onPressed: null,
-                        ),
-                        IconButton(
-                          icon: Icon( FontAwesomeIcons.linkedin,),
-                          onPressed: null,
-                        ),
-
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
+              ],
+            )
           ),
         ),
       )
+    );
+  }
+
+
+
+  _buildCard(String eventName, String org, String img, String desc){
+    return Card(
+      elevation: 10.0,
+      color: Colors.tealAccent[100],
+      shadowColor: Colors.white,
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+//                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+              children: [
+                Container(
+                  height: 100.0,
+                  width: 130.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      image: DecorationImage(
+                        image: AssetImage(img),
+                        fit: BoxFit.cover,
+                        alignment: Alignment.topLeft,
+                      )
+                  ),
+                ),
+                Column(
+                  children: [
+                    Text(eventName, style: GoogleFonts.lobster(fontSize: 20.0),),
+                    Text(org, style: GoogleFonts.lobster(fontSize: 10.0))
+                  ],
+                ),
+
+              ],
+            ),
+
+            SizedBox(height: 10.0,),
+            Text(desc, style: GoogleFonts.aBeeZee(fontSize: 18.0),),
+            FlatButton(
+              textColor: Colors.green,
+              child: Text("Read More", style: GoogleFonts.acme(fontSize: 15.0),),
+              onPressed: null,
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  icon: Icon( FontAwesomeIcons.facebook,),
+                  onPressed: null,
+                ),
+                IconButton(
+                  icon: Icon( FontAwesomeIcons.chrome,),
+                  onPressed: null,
+                ),
+                IconButton(
+                  icon: Icon( FontAwesomeIcons.twitter,),
+                  onPressed: null,
+                ),
+                IconButton(
+                  icon: Icon( FontAwesomeIcons.instagram,),
+                  onPressed: null,
+                ),
+                IconButton(
+                  icon: Icon( FontAwesomeIcons.linkedin,),
+                  onPressed: null,
+                ),
+
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
